8417a9fa19b79b5291c3b91d0e88048d87362138
AnuraghSarkar <trithatimalsina@gmail.com>
2020-09-07 16:25:55 +0545

    Some definations to work

---
+++ defines.py
@@ -0,0 +1,16 @@
+"""
+[::] in list means list[<start>:<stop>:<step>]
+"""
+
+"""
+Abstract Data types: Imperative:(mutable data type)
+            Functional:(immutable abstract data type)
+"""
+
+
+"""
+ADT--> Stack(Recursive DS LIFO  as in built-in list used in Text Editors, Reversing the characters in string!)
+ADT--> Queues (Recursive DS FIFO, order is preserved,so limited access.  adding item in queue is enqueue, removing means dequeue.
+ADT--> Deque (Recursive DS LIFO+FIFO)
+"""
+
